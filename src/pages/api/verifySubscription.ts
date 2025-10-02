155aacbf0bd8f3997c00dfdc1a0c6492e8afee47
Alger Makiputin <hello@algermakiputin.dev>
2025-10-02 17:06:27 +0800

    replace line breaks

--- src/pages/api/verifySubscription.ts
+++ src/pages/api/verifySubscription.ts
@@ -24,7 +24,7 @@ export default async function handler(
     const jwtClient = new JWT({
       // keyFile: "./service-account.json",
       email: process.env.SERVER_SIDE_CLIENT_EMAIL,
-      key: process.env.SERVER_SIDE_PRIVATE_KEY,
+      key: process.env.SERVER_SIDE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
       scopes: ["https://www.googleapis.com/auth/androidpublisher"],
     });
 
